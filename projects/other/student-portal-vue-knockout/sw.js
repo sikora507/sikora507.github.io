aae5d1378ab2765100632d3986df0e8bf393f30b
Tomasz Sikora <sikora507@gmail.com>
2019-05-09 21:32:11 +0200

    portal v2

--- projects/other/student-portal-vue-knockout/sw.js
+++ projects/other/student-portal-vue-knockout/sw.js
@@ -1,36 +1,36 @@
-importScripts('/_nuxt/workbox.4c4f5ca6.js')
+importScripts('/projects/other/student-portal-vue-knockout/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/0ed71f75c239c850c7ce.js",
+    "url": "/projects/other/student-portal-vue-knockout/_nuxt/0ed71f75c239c850c7ce.js",
     "revision": "4dc323d8d4ea90863e4b7bf9763b67d8"
   },
   {
-    "url": "/_nuxt/21aadaa1c7d200bee74b.js",
+    "url": "/projects/other/student-portal-vue-knockout/_nuxt/21aadaa1c7d200bee74b.js",
     "revision": "92ed9f31841cbf60594babc6dccd1793"
   },
   {
-    "url": "/_nuxt/5866295349b24d1b3acd.js",
+    "url": "/projects/other/student-portal-vue-knockout/_nuxt/53f7e480808e6666d1d9.js",
+    "revision": "baf115fe24d0eaaae6abac02c91987d4"
+  },
+  {
+    "url": "/projects/other/student-portal-vue-knockout/_nuxt/5866295349b24d1b3acd.js",
     "revision": "bf9d8a823d64841c6faaa979517966d0"
   },
   {
-    "url": "/_nuxt/8686d69973794d0847e7.js",
+    "url": "/projects/other/student-portal-vue-knockout/_nuxt/8686d69973794d0847e7.js",
     "revision": "09062b273f1149a087fd7433a49cc145"
   },
   {
-    "url": "/_nuxt/8a1e02b7c7ef4e59b338.js",
+    "url": "/projects/other/student-portal-vue-knockout/_nuxt/8a1e02b7c7ef4e59b338.js",
     "revision": "0651003db5294a45b8f23c3db386a605"
   },
   {
-    "url": "/_nuxt/c5ea31de68ee3a239c30.js",
-    "revision": "f1782fa453ac464ad9523c1ba5084b54"
+    "url": "/projects/other/student-portal-vue-knockout/_nuxt/97884878065ffa130655.js",
+    "revision": "f130cb3fdcd91f0c16f67dcfbba2a575"
   },
   {
-    "url": "/_nuxt/e9e8b9138e1718bb2aba.js",
-    "revision": "d9f38ebff6b003f339b39d6484b8b957"
-  },
-  {
-    "url": "/_nuxt/fd4ab1291c0653ccddfb.js",
+    "url": "/projects/other/student-portal-vue-knockout/_nuxt/fd4ab1291c0653ccddfb.js",
     "revision": "1638e780a6f07fcb5782f7f7a60e88f1"
   }
 ], {
@@ -42,6 +42,6 @@ workbox.precaching.precacheAndRoute([
 workbox.clientsClaim()
 workbox.skipWaiting()
 
-workbox.routing.registerRoute(new RegExp('/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/projects/other/student-portal-vue-knockout/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
 
-workbox.routing.registerRoute(new RegExp('/.*'), workbox.strategies.networkFirst({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/projects/other/student-portal-vue-knockout/.*'), workbox.strategies.networkFirst({}), 'GET')
